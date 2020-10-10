526a7e3365bbc9df3d28a4cb3a994b621b400644
justwait6 <2551372733@qq.com>
2020-10-10 18:32:58 +0800

    修补添加, 请无视

--- myPack.bat
+++ myPack.bat
@@ -1,6 +1,7 @@
 @echo off
 python encryptSrc.py
 cd frameworks\runtime-src\proj.android
+python build_native.py
 python build_native.py -a arm64-v8a
 call .\gradlew.bat assembleRelease --warning-mode=all
 
